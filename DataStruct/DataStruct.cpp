3ebb35daf861751daa0e5db05e1bdc5ded335cf6
JYMoon99 <97581974+JYMoon99@users.noreply.github.com>
2023-02-20 17:44:46 +0900

    (2023. 2. 17) 거품 정렬

--- DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct.cpp
@@ -1,110 +1,30 @@
 ﻿#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-struct Node
-{
-	int data;
-	Node* next;
-	Node* prev;
-};
-
-Node* head;
-Node* tail;
-
-void Insert(Node* target, int data)
-{
-	Node* newnode = new Node;
-	newnode->data = data;
-
-	newnode->prev = target;
-	newnode->next = target->next;
-
-	target->next->prev = newnode;
-	target->next = newnode;
-	
-}
-
-
-// 삭제 함수
-void Remove(Node* target)
-{
-	Node* removePtr = target->next;
-	removePtr->next->prev = target;
-	target->next = removePtr->next;
-	delete removePtr;
-
-}
-
-// 앞에서 출력하는 함수
-void FrontCircuit(Node* target)
-{
-	Node* curPtr = target->next;
-
-	while (curPtr != tail)
-	{
-		cout << curPtr->data << endl;
-		curPtr = curPtr->next;
-	}
-}
-
-void BackCircuit(Node* target)
-{
-	Node* curPtr = target->prev;
-
-	while (curPtr != head)
-	{
-		cout << curPtr->data << endl;
-		curPtr = curPtr->prev;
-	}
-}
-
-
-
 int main()
 {
-	head = new Node;
-	tail = new Node;
+	// 거품 정렬
+	// 서로 인접한 두 원소의 대소를 비교하여, 조건에 맞지 않다면
+	// 자리를 교환하여 정렬하는 알고리즘
+	int array[5] = { 7, 52, 1, 11, 3 };
+	
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 4 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(array[j], array[j + 1]);
+			}
+		}
+	}
 
-	head->prev = NULL;
-	tail->next = NULL;
-
-	head->next = tail;
-	tail->prev = head;
-	//Node* node1 = new Node;
-	//node1->data = 1000;
-	//
-	//Node* node2 = new Node;
-	//node2->data = 2000;
-	//
-	//head->next = node1;
-	//
-	//node1->prev = head;
-	//node1->next = node2;
-	//
-	//node2->prev = node1;
-	//node2->next = tail;
-	//
-	//tail->prev = node2;
-	//
-	//Node* frontPtr = head->next;
-	//
-	//while (frontPtr != tail)
-	//{
-	//	cout << frontPtr->data << endl;
-	//	frontPtr = frontPtr->next;
-	//}
-	//
-	//Node* backPtr = tail->prev;
-	//
-	//while (backPtr != head)
-	//{
-	//	cout << backPtr->data << endl;
-	//	backPtr = backPtr->prev;
-	//}
-
-	FrontCircuit(head);
-	BackCircuit(tail);
+	for (int& element : array)
+	{
+		cout << element << endl;
+	}
 
 	return 0;
 }
---
+++ DoubleLinkedLisk/DoubleLinkedList.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node* next;
+	Node* prev;
+};
+
+Node* head;
+Node* tail;
+
+void Insert(Node* target, int data)
+{
+	Node* newnode = new Node;
+	newnode->data = data;
+
+	newnode->prev = target;
+	newnode->next = target->next;
+
+	target->next->prev = newnode;
+	target->next = newnode;
+
+}
+
+
+// 삭제 함수
+void Remove(Node* target)
+{
+	Node* removePtr = target->next;
+
+	if (removePtr == tail)
+	{
+		cout << "현재 리스트에 Node가 없습니다." << endl;
+		return;
+	}
+
+	removePtr->next->prev = target;
+
+	target->next = removePtr->next;
+
+	delete removePtr;
+}
+
+// 앞에서 출력하는 함수
+void FrontCircuit(Node* target)
+{
+	Node* curPtr = target->next;
+
+	while (curPtr != tail)
+	{
+		cout << curPtr->data << endl;
+		curPtr = curPtr->next;
+	}
+}
+
+void BackCircuit(Node* target)
+{
+	Node* curPtr = target->prev;
+
+	while (curPtr != head)
+	{
+		cout << curPtr->data << endl;
+		curPtr = curPtr->prev;
+	}
+}
+
+
+
+int main()
+{
+	head = new Node;
+	tail = new Node;
+
+	head->prev = NULL;
+	tail->next = NULL;
+
+	head->next = tail;
+	tail->prev = head;
+	//Node* node1 = new Node;
+	//node1->data = 1000;
+	//
+	//Node* node2 = new Node;
+	//node2->data = 2000;
+	//
+	//head->next = node1;
+	//
+	//node1->prev = head;
+	//node1->next = node2;
+	//
+	//node2->prev = node1;
+	//node2->next = tail;
+	//
+	//tail->prev = node2;
+	//
+	//Node* frontPtr = head->next;
+	//
+	//while (frontPtr != tail)
+	//{
+	//	cout << frontPtr->data << endl;
+	//	frontPtr = frontPtr->next;
+	//}
+	//
+	//Node* backPtr = tail->prev;
+	//
+	//while (backPtr != head)
+	//{
+	//	cout << backPtr->data << endl;
+	//	backPtr = backPtr->prev;
+	//}
+
+	FrontCircuit(head);
+	BackCircuit(tail);
+
+	return 0;
+}
