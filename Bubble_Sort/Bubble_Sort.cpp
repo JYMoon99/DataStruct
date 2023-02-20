b30ebae540244c98d324c9f84d08e504392f04ed
JYMoon99 <97581974+JYMoon99@users.noreply.github.com>
2023-02-20 19:47:38 +0900

    (2023. 2. 20) 선택 정렬

---
+++ Bubble_Sort/Bubble_Sort.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	// 거품 정렬
+	// 서로 인접한 두 원소의 대소를 비교하여, 조건에 맞지 않다면
+	// 자리를 교환하여 정렬하는 알고리즘
+	int array[5] = { 7, 52, 1, 11, 3 };
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 4 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(array[j], array[j + 1]);
+			}
+		}
+	}
+
+	for (int& element : array)
+	{
+		cout << element << endl;
+	}
+
+	return 0;
+}
--- DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct.cpp
@@ -1,30 +1,72 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS // 문자열 관련 함수 보안 경고로 인한 컴파일 에러 방지
+#include <iostream>
 #include <algorithm>
+
 
 using namespace std;
 
+// 문자열 관련 함수 (strcpy)
+class Person
+{
+public :
+	const char* name;
+	char nickName[20];
+};
+
+
 int main()
 {
-	// 거품 정렬
-	// 서로 인접한 두 원소의 대소를 비교하여, 조건에 맞지 않다면
-	// 자리를 교환하여 정렬하는 알고리즘
-	int array[5] = { 7, 52, 1, 11, 3 };
+	// 문자열 복사 함수
+	/*
+	Person person;
+
+	// 상수 지시 포인터
+	person.name = "kim";
+	person.name = "Lee";
+
+	char temp[20] = "Faker";
+	char temp1[20] = "MadLife";
+	// 배열의 이름은 배열의 시작 주소를 의미
+
+	// 포인터 상수
+	// [F][a][k][e][r][\0] <- [F][a][k][e][r]
+	// strcpy
+	// 첫 번쨰 매개변수 : 복사받을 문자 배열
+	// 두 번째 매개변수 : 복사시킬 문자 배열
+
+	strcpy(person.nickName, temp);
+	strcpy(person.nickName, temp1);
+
+	cout << "person.nickName : " << person.nickName << endl;
+	*/
+
+	// 선택 정렬
+	// 주어진 리스트중에서 최솟값을 찾은 다음 그 값을 맨 앞에
+	// 위치한 값과 교체하는 정렬
+
+	int array[5] = { 15, 3, 11, 1, 4};
 	
 	for (int i = 0; i < 5; i++)
 	{
-		for (int j = 0; j < 4 - i; j++)
+		int minIndex = i;
+
+		for (int j = i; j < 5; j++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[minIndex] > array[j])
 			{
-				swap(array[j], array[j + 1]);
+				minIndex = j;
 			}
 		}
+
+		swap(array[minIndex], array[i]);
 	}
 
-	for (int& element : array)
+	for (const int& element : array)
 	{
 		cout << element << endl;
 	}
+
+	// 시간 복잡도 O(n^2)
 
 	return 0;
 }
